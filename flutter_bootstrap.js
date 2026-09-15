5e7c1aaca15b58719734e0868e1c09f02d47f0e6
FitForge Dev <fitforge@dev.local>
2026-09-16 00:38:11 +0200

    Deploy: theme clair/sombre + nouveaux sons de minuteur

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3640900632" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1221008913" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
